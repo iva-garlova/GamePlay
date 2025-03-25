2b9084af4de2bb571b22897b2ffdc9d357969fa3
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-03-25 15:07:02 +0200

    when there are no items page

--- client/src/components/games/Games.jsx
+++ client/src/components/games/Games.jsx
@@ -17,11 +17,10 @@ export default function Games() {
     return (
         <section id="catalog-page">
         <h1>All Games</h1>
-        {games.map(game => <GameCatalog key={game._id} {...game} />)}
-      
 
-        {/* <!-- Display paragraph: If there is no games  --> */}
-        <h3 className="no-articles">No articles yet</h3>
+      {games.length > 0
+      ? games.map(game => <GameCatalog key={game._id} {...game} />)
+     : <h3 className="no-articles">No articles yet</h3>}
     </section>
     );
 }

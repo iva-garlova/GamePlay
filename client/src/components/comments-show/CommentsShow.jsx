37c2b5396dd9de2fefabba80b492d85bbd1cb9c8
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-03-25 16:47:31 +0200

    making the comments in separate components

---
+++ client/src/components/comments-show/CommentsShow.jsx
@@ -0,0 +1,16 @@
+export default function CommentsShow() {
+    return (
+        <div className="details-comments" >
+            <h2>Comments:</h2>
+            <ul>
+                <li className="comment">
+                    <p>Content: I rate this one quite highly.</p>
+                </li>
+                <li className="comment">
+                    <p>Content: The best game.</p>
+                </li>
+            </ul>
+            <p className="no-comment">No comments.</p>
+        </div >
+    );
+}

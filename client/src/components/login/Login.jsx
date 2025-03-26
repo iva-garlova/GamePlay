6d2a34ddc304f347b8cced6201f86f2344c55b33
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-03-26 16:18:09 +0200

    Update Login.jsx

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,14 +1,23 @@
+import { useActionState } from 'react';
 import { Link, useNavigate } from 'react-router'
+
 export default function  Login({
     onLogin,
 }) {
     const navigate = useNavigate();
-    const loginAction = (formData)=>{
-      const email = formData.get('email');
-      onLogin(email);
-      navigate('/games')
+
+    const loginHandler = (previousState, formData)=>{
+      const values = Object.fromEntries(formData)
+
+      onLogin(values.email);
+
+      navigate('/games');
+      return values;
       
     };
+    const [values, loginAction, isPending ] = useActionState(loginHandler, {email:'', password:''});
+    console.log(values);
+    
     
     return (
         <section id="login-page" className="auth">
@@ -22,7 +31,7 @@ export default function  Login({
 
                 <label htmlfor="login-pass">Password:</label>
                 <input type="password" id="login-password" name="password"/>
-                <input type="submit" className="btn submit" value="Login"/>
+                <input type="submit" className="btn submit" value="Login" disabled={isPending}/>
                 <p className="field">
                     <span>If you don't have profile click <Link to="/register">here</Link></span>
                 </p>
